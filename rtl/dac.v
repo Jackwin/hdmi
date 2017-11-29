d6ee29878cb0f9d601c1b633d077f3fe72ccde42
Chunjie Wang <Chunjie.wang@intel.com>
2017-11-29 09:46:06 +0800

    1. Code in dac.v 2. Add sin.dat with 20 samples of sine datawq

--- rtl/dac.v
+++ rtl/dac.v
@@ -6,4 +6,29 @@ module dac (
 
 );
 
+reg [13:0] sin_mem [0:19];
+reg [4:0] cnt;
+initial begin
+    $readmemh("sin.dat",sin_mem);
+end
+
+always @(posedge ref_clk) begin
+    if (~reset_n) begin
+        dac_data <= 0;
+        cnt <= 0;
+    end
+    else begin
+        if (cnt == 4'd19) begin
+            cnt <= 0;
+        end
+        else begin
+            cnt <= cnt + 1;
+        end
+    end
+end
+
+assign dac_data = sin_mem[cnt];
+
+
+
 endmodule
